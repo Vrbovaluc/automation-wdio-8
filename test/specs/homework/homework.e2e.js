b2e3dce328f06b849420b2f4fe791e72af51a6b4
Vrbovalucie <vrbova.luc@gmail.com>
2023-06-09 15:29:23 +0200

    Homework_03

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -1,8 +1,29 @@
-describe('Homework', async () => {
+import {username, password} from '../fixtures.js'
 
-    it('should open page and create screenshot', async () => {
+describe('Should log in', async () => {
 
-        // sem vypracuj domácí úkol
+    beforeEach (async () => {
+        await browser.reloadSession();
+        await browser.url('/registrace');
+    });
+
+    afterEach(async () => {
+        await browser.pause(5000);
+    });
+
+    it('should log in', async () => {
+
+        const nameField = $('#name');
+        const emailField = $('#email');
+        const passwordField = $('#password');
+        const passwordConfirmField = $('#password-confirm');
+        const loginButton = $('.btn-primary');
+
+        await nameField.setValue('Eliška Krásná');
+        await emailField.setValue(username);
+        await passwordField.setValue(password);
+        await passwordConfirmField.setValue(password);
+        await loginButton.click();
 
     });
 
